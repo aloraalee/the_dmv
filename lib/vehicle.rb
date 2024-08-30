ab4988bfd9e1ffdc1b31dd7fa0fbde0c34ddaecc
Alora Riley <alora.riley@gmail.com>
2024-08-30 14:07:17 -0600

    Add plate_type method

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -5,8 +5,8 @@ class Vehicle
               :year,
               :make,
               :model,
-              :engine,
-              :registration_date
+              :engine
+  attr_accessor :registration_date, :plate_type
 
   def initialize(vehicle_details)
     @vin = vehicle_details[:vin]
@@ -14,7 +14,7 @@ class Vehicle
     @make = vehicle_details[:make]
     @model = vehicle_details[:model]
     @engine = vehicle_details[:engine]
-    @registration_date = registration_date
+    @registration_date = nil
   end
 
   def antique?
@@ -24,4 +24,18 @@ class Vehicle
   def electric_vehicle?
     @engine == :ev
   end
+
+  def plate_type 
+    if antique?
+      :antique
+    elsif electric_vehicle?
+      :ev
+    else 
+      :regular
+    end
+  end 
+
 end
+
+
+#    require 'pry';binding.pry
