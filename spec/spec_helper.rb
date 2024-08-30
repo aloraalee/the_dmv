87eb839ecfebd0e32bb7414dbb10dc28774cb495
Alora Riley <alora.riley@gmail.com>
2024-08-30 14:10:00 -0600

    Add require date class

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,10 @@ require './lib/facility'
 require './lib/vehicle'
 require './lib/dmv_data_service'
 require './lib/registrant'
+require 'date'
 
 RSpec.configure do |config|
     config.formatter = :documentation
 end
+
+
