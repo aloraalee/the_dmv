29de841ec56d4f3885ab1d1d6ea887847ba2a11c
Alora Riley <alora.riley@gmail.com>
2024-08-31 17:35:23 -0600

    Add create vehicles method

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -1,19 +1,19 @@
-class ExternalVehicleDataWA
-    attr_reader :vin,
-    :year,
-    :make,
-    :model,
-    :engine
-    attr_accessor :registration_date, :plate_type
+class VehicleFactory
 
-    def initialize(vehicle_details)
-        @vin = vehicle_details[:vin]
-        @year = vehicle_details[:year]
-        @make = vehicle_details[:make]
-        @model = vehicle_details[:model]
-        @engine = vehicle_details[:engine]
-        @registration_date = nil
-    end
+    def initialize
+    end    
 
+    def create_vehicles(registrations)
+        registrations.map do |registration|
+            Vehicle.new(
+                vin: registration[:vin_1_10],
+                year: registration[:model_year].to_i,
+                make: registration[:make],
+                model: registration[:model],
+                engine: :ev)
+                # plate_type: nil,
+                # registration_date: nil)
+        end
+    end 
 
 end
