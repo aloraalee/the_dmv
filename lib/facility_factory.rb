0a76fe78a934c781911f0299d75e10ac8c4ba82e
Alora Riley <alora.riley@gmail.com>
2024-09-02 13:42:00 -0600

    Add create facilities method that uses dmv data service

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -3,4 +3,15 @@ class FacilityFactory
     def initialize
     end    
 
+    def create_facilities(office_locations)
+        office_locations.map do |office_location|
+            Facility.new(
+                name: office_location[:dmv_office],
+                address: office_location[:address_li],
+                phone: office_location[:phone],
+                services: office_location[:services_p]
+            )
+        end
+    end 
+
 end
