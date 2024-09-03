b09f1542ee8732f4b3cfa99e03ce4d14fc00a6eb
Alora Riley <alora.riley@gmail.com>
2024-09-03 13:25:07 -0600

    Add full address methods for all states

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -7,31 +7,66 @@ class FacilityFactory
         office_locations.map do |office_location|
             Facility.new(
                 name: office_location[:dmv_office],
-                address: office_location[:address_li],
+                address: build_full_address_co(office_location),
                 phone: office_location[:phone],
                 services: office_location[:services_p]
             )
         end
     end 
    
+    def build_full_address_co(office_location)
+        address_parts = []
+        address_parts.push(
+            office_location[:address_li],
+            office_location[:address__1],
+            office_location[:city],
+            office_location[:state],
+            office_location[:zip]
+        )
+        address_parts.compact.join(', ')
+    end
+
     def create_ny_facilities(office_locations)
         office_locations.map do |office_location|
             Facility.new(
                 name: office_location[:office_name],
-                address: office_location[:street_address_line_1],
+                address: build_full_address_ny(office_location),
                 phone: office_location[:public_phone_number],
             )
         end
     end 
 
+    def build_full_address_ny(office_location)
+        address_parts = []
+        address_parts.push(
+            office_location[:street_address_line_1],
+            office_location[:city],
+            office_location[:state],
+            office_location[:zip_code]
+        )
+        address_parts.compact.join(', ')
+    end
+
     def create_mo_facilities(office_locations)
         office_locations.map do |office_location|
             Facility.new(
                 name: office_location[:name],
-                address: office_location[:address1],
+                address: build_full_address_mo(office_location),
                 phone: office_location[:phone],
             )
         end
     end 
+
+    def build_full_address_mo(office_location)
+        address_parts = []
+        address_parts.push(
+            office_location[:address1],
+            office_location[:city],
+            office_location[:state],
+            office_location[:zipcode]
+        )
+        address_parts.compact.join(', ')
+    end
+
 
 end
